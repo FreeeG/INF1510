e3c8586e365d10257240c5c3206f78211dd7d415
Fredrik Glendrange <Fredrik@Glendrange.com>
2016-04-16 19:02:28 +0200

    Update vu-6LED-Vibrator.ino

--- vu-6LED-Vibrator.ino
+++ vu-6LED-Vibrator.ino
@@ -1,4 +1,7 @@
 //16.April
+//Vibrator pulsering
+//Dimbar lysrekke med 6 leds 3 gronne 2 gule 1 rod.
+
 const int rod = 11;  
 const int gul2 = 10;
 const int gul1 = 9;
@@ -7,48 +10,43 @@ const int gronn2 = 5;
 const int gronn1 = 3;
 const int vibrator = A0;
 
-
 void setup() {
+  pulse(vibrator, 3); //kort pulse av vibratoren
   pulse(vibrator, 3);
   pulse(vibrator, 3);
-  pulse(vibrator, 3);
-  delay(5000);
+  delay(5000); //vent 5 sekunder
   increase(gronn1,80);
   increase(gronn2,25);
   increase(gronn3,27);
   increase(gul1,30);
   increase(gul2,40);
   increase(rod,45);
-  // nothing happens in setup
 }
 
-void pulse(int a, int b){
+void pulse(int a, int b){ // dim opp og ned a=hva b = delay
   increase(a,b);
+  delay(30);
   decrease(a,b);
 }
 
-void increase(int a, int b){
+void increase(int a, int b){ //oek a(hva) delay paa B
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
     analogWrite(a, fadeValue);
     delay(b);
   } 
 }
 
-void decrease(int a, int b){
+void decrease(int a, int b){ //senk a(hva) delay paa B
     for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
     analogWrite(a, fadeValue);
     delay(b);
   }
 }
 
-void on(int a) {
+void on(int a) { // Skru paa A
   digitalWrite(a, HIGH);
 }
 
 void loop() {
-  // fade in from min to max in increments of 5 points:
-  
-
-  // fade out from max to min in increments of 5 points:
 
 }
